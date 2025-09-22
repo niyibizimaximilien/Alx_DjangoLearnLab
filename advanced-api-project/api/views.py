9c946b0c96148dff1e1511baa66e9e4d4b142c16
NIYIBIZI Maximilien <2kapomaxime@gmail.com>
2025-09-22 12:49:06 +0200

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,48 +1,74 @@
-from rest_framework import generics
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Author, Book
-from .serializers import AuthorSerializer, BookSerializer
 
 # --------------------
 # Author Views
 # --------------------
 
-class AuthorListView(generics.ListCreateAPIView):
-    """
-    GET: List all authors
-    POST: Create a new author
-    """
-    queryset = Author.objects.all()
-    serializer_class = AuthorSerializer
+class AuthorListView(ListView):
+    model = Author
+    template_name = 'authors/author_list.html'
+    context_object_name = 'authors'
 
 
-class AuthorDetailView(generics.RetrieveUpdateDestroyAPIView):
-    """
-    GET: Retrieve an author
-    PUT/PATCH: Update an author
-    DELETE: Delete an author
-    """
-    queryset = Author.objects.all()
-    serializer_class = AuthorSerializer
+class AuthorDetailView(DetailView):
+    model = Author
+    template_name = 'authors/author_detail.html'
+    context_object_name = 'author'
+
+
+class AuthorCreateView(CreateView):
+    model = Author
+    fields = ['name']
+    template_name = 'authors/author_form.html'
+    success_url = reverse_lazy('author-list')
+
+
+class AuthorUpdateView(UpdateView):
+    model = Author
+    fields = ['name']
+    template_name = 'authors/author_form.html'
+    success_url = reverse_lazy('author-list')
+
+
+class AuthorDeleteView(DeleteView):
+    model = Author
+    template_name = 'authors/author_confirm_delete.html'
+    success_url = reverse_lazy('author-list')
 
 
 # --------------------
 # Book Views
 # --------------------
 
-class BookListView(generics.ListCreateAPIView):
-    """
-    GET: List all books
-    POST: Create a new book
-    """
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
+class BookListView(ListView):
+    model = Book
+    template_name = 'books/book_list.html'
+    context_object_name = 'books'
 
 
-class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
-    """
-    GET: Retrieve a book
-    PUT/PATCH: Update a book
-    DELETE: Delete a book
-    """
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
+class BookDetailView(DetailView):
+    model = Book
+    template_name = 'books/book_detail.html'
+    context_object_name = 'book'
+
+
+class BookCreateView(CreateView):
+    model = Book
+    fields = ['title', 'publication_year', 'author']
+    template_name = 'books/book_form.html'
+    success_url = reverse_lazy('book-list')
+
+
+class BookUpdateView(UpdateView):
+    model = Book
+    fields = ['title', 'publication_year', 'author']
+    template_name = 'books/book_form.html'
+    success_url = reverse_lazy('book-list')
+
+
+class BookDeleteView(DeleteView):
+    model = Book
+    template_name = 'books/book_confirm_delete.html'
+    success_url = reverse_lazy('book-list')
