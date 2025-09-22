99c810b595bf3f47196f5c27bd84ecd020482887
maximilien <niyibizimaximilien@gmail.com>
2025-09-22 12:04:55 +0200

    oh my GOD i love Django

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Author, Book
+from .serializers import AuthorSerializer, BookSerializer
 
-# Create your views here.
+# --------------------
+# Author Views
+# --------------------
+
+class AuthorListView(generics.ListCreateAPIView):
+    """
+    GET: List all authors
+    POST: Create a new author
+    """
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+
+class AuthorDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    GET: Retrieve an author
+    PUT/PATCH: Update an author
+    DELETE: Delete an author
+    """
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+
+# --------------------
+# Book Views
+# --------------------
+
+class BookListView(generics.ListCreateAPIView):
+    """
+    GET: List all books
+    POST: Create a new book
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    GET: Retrieve a book
+    PUT/PATCH: Update a book
+    DELETE: Delete a book
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
