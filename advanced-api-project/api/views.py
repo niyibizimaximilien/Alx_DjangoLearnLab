eea5491e517d5a62623d0b0f5cd1fa27eb165708
NIYIBIZI Maximilien <2kapomaxime@gmail.com>
2025-09-22 13:18:34 +0200

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,16 +1,76 @@
-from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter, OrderingFilter
-from .models import Book
-from .serializers import BookSerializer
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
-class BookListView(generics.ListAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
-    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
-    filterset_fields = ['title', 'publication_year', 'author']
-    search_fields = ['title', 'author__name']
-    ordering_fields = ['title', 'publication_year']
-    ordering = ['title']
+from .models import Author, Book
+
+# --------------------
+# Author Views
+# --------------------
+
+class AuthorListView(ListView):
+    model = Author
+    template_name = 'authors/author_list.html'
+    context_object_name = 'authors'
+
+
+class AuthorDetailView(DetailView):
+    model = Author
+    template_name = 'authors/author_detail.html'
+    context_object_name = 'author'
+
+
+class AuthorCreateView(CreateView):
+    model = Author
+    fields = ['name']
+    template_name = 'authors/author_form.html'
+    success_url = reverse_lazy('author-list')
+
+
+class AuthorUpdateView(UpdateView):
+    model = Author
+    fields = ['name']
+    template_name = 'authors/author_form.html'
+    success_url = reverse_lazy('author-list')
+
+
+class AuthorDeleteView(DeleteView):
+    model = Author
+    template_name = 'authors/author_confirm_delete.html'
+    success_url = reverse_lazy('author-list')
+
+
+# --------------------
+# Book Views
+# --------------------
+
+class BookListView(ListView):
+    model = Book
+    template_name = 'books/book_list.html'
+    context_object_name = 'books'
+
+
+class BookDetailView(DetailView):
+    model = Book
+    template_name = 'books/book_detail.html'
+    context_object_name = 'book'
+
+
+class BookCreateView(CreateView):
+    model = Book
+    fields = ['title', 'publication_year', 'author']
+    template_name = 'books/book_form.html'
+    success_url = reverse_lazy('book-list')
+
+
+class BookUpdateView(UpdateView):
+    model = Book
+    fields = ['title', 'publication_year', 'author']
+    template_name = 'books/book_form.html'
+    success_url = reverse_lazy('book-list')
+
+
+class BookDeleteView(DeleteView):
+    model = Book
+    template_name = 'books/book_confirm_delete.html'
+    success_url = reverse_lazy('book-list')
