08f26eab082b00428e2d682f411203f5bdfdaad5
maximilien <niyibizimaximilien@gmail.com>
2025-09-22 12:50:20 +0200

    oh my GOD i love Django

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,74 +1,21 @@
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.urls import reverse_lazy
-from .models import Author, Book
+from django.urls import path
+from .views import (
+    AuthorListView, AuthorDetailView, AuthorCreateView, AuthorUpdateView, AuthorDeleteView,
+    BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView
+)
 
-# --------------------
-# Author Views
-# --------------------
+urlpatterns = [
+    # Author URLs
+    path('authors/', AuthorListView.as_view(), name='author-list'),
+    path('authors/<int:pk>/', AuthorDetailView.as_view(), name='author-detail'),
+    path('authors/create/', AuthorCreateView.as_view(), name='author-create'),
+    path('authors/<int:pk>/update/', AuthorUpdateView.as_view(), name='author-update'),
+    path('authors/<int:pk>/delete/', AuthorDeleteView.as_view(), name='author-delete'),
 
-class AuthorListView(ListView):
-    model = Author
-    template_name = 'authors/author_list.html'
-    context_object_name = 'authors'
-
-
-class AuthorDetailView(DetailView):
-    model = Author
-    template_name = 'authors/author_detail.html'
-    context_object_name = 'author'
-
-
-class AuthorCreateView(CreateView):
-    model = Author
-    fields = ['name']
-    template_name = 'authors/author_form.html'
-    success_url = reverse_lazy('author-list')
-
-
-class AuthorUpdateView(UpdateView):
-    model = Author
-    fields = ['name']
-    template_name = 'authors/author_form.html'
-    success_url = reverse_lazy('author-list')
-
-
-class AuthorDeleteView(DeleteView):
-    model = Author
-    template_name = 'authors/author_confirm_delete.html'
-    success_url = reverse_lazy('author-list')
-
-
-# --------------------
-# Book Views
-# --------------------
-
-class BookListView(ListView):
-    model = Book
-    template_name = 'books/book_list.html'
-    context_object_name = 'books'
-
-
-class BookDetailView(DetailView):
-    model = Book
-    template_name = 'books/book_detail.html'
-    context_object_name = 'book'
-
-
-class BookCreateView(CreateView):
-    model = Book
-    fields = ['title', 'publication_year', 'author']
-    template_name = 'books/book_form.html'
-    success_url = reverse_lazy('book-list')
-
-
-class BookUpdateView(UpdateView):
-    model = Book
-    fields = ['title', 'publication_year', 'author']
-    template_name = 'books/book_form.html'
-    success_url = reverse_lazy('book-list')
-
-
-class BookDeleteView(DeleteView):
-    model = Book
-    template_name = 'books/book_confirm_delete.html'
-    success_url = reverse_lazy('book-list')
+    # Book URLs
+    path('books/', BookListView.as_view(), name='book-list'),
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
+    path('books/create/', BookCreateView.as_view(), name='book-create'),
+    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
+    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
+]
