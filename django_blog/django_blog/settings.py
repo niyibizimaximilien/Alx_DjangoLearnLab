060989a49951b84f86c64757011ca790116e0a34
maximilien <niyibizimaximilien@gmail.com>
2025-10-01 10:39:21 +0200

    .

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -96,8 +96,12 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
