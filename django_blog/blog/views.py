7797b12f0108eab83f0ec041f6a385361bcff4ca
maximilien <niyibizimaximilien@gmail.com>
2025-10-01 11:09:25 +0200

    Add Post model and update settings for user profiles

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -9,6 +9,56 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 from .forms import PostForm
+from django.shortcuts import get_object_or_404, redirect
+from django.views.generic import View
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .models import Comment
+from .forms import CommentForm
+
+# Add comment
+class CommentCreateView(LoginRequiredMixin, View):
+    def post(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+        return redirect('blog:post-detail', pk=pk)
+
+# Edit comment
+class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, View):
+    def get(self, request, pk):
+        comment = get_object_or_404(Comment, pk=pk)
+        form = CommentForm(instance=comment)
+        return render(request, 'blog/comment_form.html', {'form': form})
+
+    def post(self, request, pk):
+        comment = get_object_or_404(Comment, pk=pk)
+        form = CommentForm(request.POST, instance=comment)
+        if form.is_valid() and self.test_func():
+            form.save()
+            return redirect('blog:post-detail', pk=comment.post.pk)
+        return render(request, 'blog/comment_form.html', {'form': form})
+
+    def test_func(self):
+        comment = get_object_or_404(Comment, pk=self.kwargs['pk'])
+        return self.request.user == comment.author
+
+# Delete comment
+class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, View):
+    def post(self, request, pk):
+        comment = get_object_or_404(Comment, pk=pk)
+        post_pk = comment.post.pk
+        if self.test_func():
+            comment.delete()
+        return redirect('blog:post-detail', pk=post_pk)
+
+    def test_func(self):
+        comment = get_object_or_404(Comment, pk=self.kwargs['pk'])
+        return self.request.user == comment.author
+
 def register_view(request):
     if request.user.is_authenticated:
         return redirect('blog:profile')
