0877d9d7750093641e0f76c900b8c27195529b2c
maximilien <niyibizimaximilien@gmail.com>
2025-10-01 11:08:25 +0200

    Add Post model and update settings for user profiles

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -3,6 +3,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Profile
 from .models import Post
+from .models import Comment
+
 
 
 class RegisterForm(UserCreationForm):
@@ -28,3 +30,12 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content']
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+        widgets = {
+            'content': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Write your comment here...'})
+        }
