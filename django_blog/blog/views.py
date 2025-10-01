a6042997d53d43ba1136fcc20a5266c96d217f46
maximilien <niyibizimaximilien@gmail.com>
2025-10-01 10:44:25 +0200

    Add Post model and update settings for user profiles

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import RegisterForm, UserUpdateForm, ProfileUpdateForm
 
-# Create your views here.
+def register_view(request):
+    if request.user.is_authenticated:
+        return redirect('blog:profile')
+
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # optional: automatically log in after register
+            login(request, user)
+            messages.success(request, "Account created. Welcome!")
+            return redirect('blog:profile')
+        else:
+            messages.error(request, "Please fix the errors below.")
+    else:
+        form = RegisterForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+from django.contrib.auth.views import LoginView, LogoutView
+
+class CustomLoginView(LoginView):
+    template_name = 'blog/login.html'
+
+class CustomLogoutView(LogoutView):
+    template_name = 'blog/logout.html'  # optional, Django can redirect to LOGOUT_REDIRECT_URL
+
+@login_required
+def profile_view(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, "Profile updated.")
+            return redirect('blog:profile')
+        else:
+            messages.error(request, "Please fix errors below.")
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request, 'blog/profile.html', context)
