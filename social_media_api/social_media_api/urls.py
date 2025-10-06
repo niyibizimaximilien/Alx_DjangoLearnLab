f85a69f4635d78cd212af433a566fc516561dd2a
maximilien <niyibizimaximilien@gmail.com>
2025-10-06 09:46:12 +0200

    .

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -19,4 +19,6 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/accounts/', include('accounts.urls')),
+    path('api/', include('posts.urls')),
 ]
